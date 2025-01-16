246426e6f3ff43652b530156525870e9347c3c96
bryan.qurniawan <bryan.qurniawan@weekendinc.com>
2025-01-17 06:32:16 +0700

    update

--- src/components/ChatWindow.jsx
+++ src/components/ChatWindow.jsx
@@ -162,9 +162,9 @@ function ChatWindow({
       {/* Messages */}
       <div
         ref={chatContainerRef}
-        className="flex-1 overflow-y-auto  rounded-t-[28px] relative z-[3] mt-[80px] pb-[80px]"
+        className="flex-1 overflow-y-auto  rounded-t-[28px] relative z-[4] mt-[80px] pb-[80px]"
       >
-        <div className="max-w-3xl mx-auto px-4 py-6 space-y-6 relative z-[4]">
+        <div className="max-w-3xl mx-auto px-4 py-6 space-y-6 relative z-[3]">
           {messages.map((msg, idx) => (
             <div
               key={idx}
